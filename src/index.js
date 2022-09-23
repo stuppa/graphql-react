b9c9499bc4da9a2c133d0e7aafcd98a64d7fff5a
rafael Contreras <stupy4444caca@gmail.com>
2022-09-23 00:50:59 -0500

    first jijijijij

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8080'
+});
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+/*
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+*/
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<ApolloProvider client={client}>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+</ApolloProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
